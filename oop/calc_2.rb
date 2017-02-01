2a3ff9e407d50541d492ff03326fa51b4a48959b
John Messina <johnvm914@gmail.com>
2017-02-01 12:52:13 -0500

    submit calc_2.rb and calc_3.rb solutions

--- oop/calc_2.rb
+++ oop/calc_2.rb
@@ -2,9 +2,7 @@
 # by using Inheritance. In this exercise, you must DRY up your code 
 # a different technique using Composition. Hint: Google 'Ruby Mixin Module'. (Using
 # mixin modules is how you achieve Composition with Ruby.)
-
-class SimpleCalculator
-
+module Calculator
   def add(first_number, second_number)
     first_number + second_number
   end
@@ -20,32 +18,50 @@ class SimpleCalculator
   def divide(first_number, second_number)
     first_number / second_number
   end
+end
 
+class SimpleCalculator
+  include Calculator
 end
 
 class FancyCalculator
-
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
+  include Calculator
 
   def square_root(number)
     Math.sqrt(number)
   end
-
 end
 
 # Copy your driver code from the previous exercise below:
+calc1 = SimpleCalculator.new
+calc2 = FancyCalculator.new
 
+# Test Simple Calculator (module mixin)
+result = calc1.add(2, 3)
+puts "Your result returned:"
+puts result
+if result == 5
+  puts "PASS!"
+else
+  puts "F"
+end
+
+# Test Fancy Calculator
+result = calc2.square_root(144)
+puts "Your result returned:"
+puts result
+if result == 12
+  puts "PASS!"
+else
+  puts "F"
+end
+
+# Test Fancy Calculator (module mixin)
+result = calc2.multiply(6, 3)
+puts "Your result returned:"
+puts result
+if result == 18
+  puts "PASS!"
+else
+  puts "F"
+end
--- oop/calc_3.rb
+++ oop/calc_3.rb
@@ -24,23 +24,7 @@ class SimpleCalculator
 
 end
 
-class FancyCalculator
-
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
+class FancyCalculator < SimpleCalculator
 
   def square_root(number)
     Math.sqrt(number)
@@ -48,27 +32,7 @@ class FancyCalculator
 
 end
 
-class WhizBangCalculator
-
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
-
-  def square_root(number)
-    Math.sqrt(number)
-  end
+class WhizBangCalculator < FancyCalculator
 
   def hypotenuse(first_number, second_number)
     Math.hypot(first_number, second_number)
@@ -83,4 +47,60 @@ class WhizBangCalculator
 end
 
 # Copy your driver code from the previous exercise and more below:
+# Write your own driver code below:
+calc1 = SimpleCalculator.new
+calc2 = FancyCalculator.new
+calc3 = WhizBangCalculator.new
+
+# Test Simple Calculator
+result = calc1.add(2, 3)
+puts "Your result returned:"
+puts result
+if result == 5
+  puts "PASS!"
+else
+  puts "F"
+end
+
+# Test Fancy Calculator
+result = calc2.square_root(144)
+puts "Your result returned:"
+puts result
+if result == 12
+  puts "PASS!"
+else
+  puts "F"
+end
+
+# Test Fancy Calculator Inheritance
+result = calc2.multiply(6, 3)
+puts "Your result returned:"
+puts result
+if result == 18
+  puts "PASS!"
+else
+  puts "F"
+end
+
+# Test Whiz Bang Calculator
+result = calc3.exponent(2, 4)
+puts "Your result returned:"
+puts result
+if result == 16
+  puts "PASS!"
+else
+  puts "F"
+end
+
+# Test Whiz Bang Calculator Inheritance
+result = calc3.divide(18, 3)
+puts "Your result returned:"
+puts result
+if result == 6
+  puts "PASS!"
+else
+  puts "F"
+end
+
+
 
